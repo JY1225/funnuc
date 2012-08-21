311910474216d4cc90905f8ce29c0d2a581d3624
Peter De Roovere <peter@robojob.eu>
2012-08-21 14:36:17 +0000

    First test succeeded!

--- DeviceSimulator/src/eu/robojob/devicesimulator/cncmillingmachine/CNCMillingMachineSimulator.java
+++ DeviceSimulator/src/eu/robojob/devicesimulator/cncmillingmachine/CNCMillingMachineSimulator.java
@@ -11,7 +11,7 @@ import eu.robojob.irscw.external.communication.SocketConnection;
 
 public class CNCMillingMachineSimulator {
 	
-	private static int portNumber = 49153;
+	private static int portNumber = 1234;
 	private static Logger logger = Logger.getLogger(CNCMillingMachineSimulator.class);
 	
 	public static void main(String[] args) {
@@ -19,11 +19,13 @@ public class CNCMillingMachineSimulator {
 		try {
 			ServerSocket serverSocket = new ServerSocket(portNumber);
 			logger.debug("created new serversocket on port: " + portNumber);
-			Socket socket = serverSocket.accept();
-			logger.debug("new socket-connection!");
-			SocketConnection socketConnection = new SocketConnection("CNC Milling Machine connection", socket);
-			WaitAndRespondThread thread = new WaitAndRespondThread(socketConnection, 2000);
-			thread.run();
+			while(true) {
+				Socket socket = serverSocket.accept();
+				logger.debug("new socket-connection!");
+				SocketConnection socketConnection = new SocketConnection("CNC Milling Machine connection", socket);
+				WaitAndRespondThread thread = new WaitAndRespondThread(socketConnection, 2000);
+				thread.run();
+			}
 		} catch (IOException e) {
 			logger.error(e);
 		}
