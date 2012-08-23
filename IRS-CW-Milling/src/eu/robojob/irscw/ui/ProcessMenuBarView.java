ec9929c8d8d433539ea4ab320bb8601dba200989
Peter De Roovere <peter@robojob.eu>
2012-08-23 15:31:22 +0000

    Adjustments to buttonbar

--- IRS-CW-Milling/src/eu/robojob/irscw/ui/ProcessMenuBarView.java
+++ IRS-CW-Milling/src/eu/robojob/irscw/ui/ProcessMenuBarView.java
@@ -2,16 +2,19 @@ package eu.robojob.irscw.ui;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.HPos;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.ToolBar;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
+import eu.robojob.irscw.util.Translator;
 
-public class ProcessMenuBarView extends HBox {
+public class ProcessMenuBarView extends ToolBar {
 	
 	private Button btnAlarms;
 	private Button btnAdmin;
+	
+	private HBox buttonBar;
 	
 	private HBox hBoxProcessMenuItems;
 	private Button btnConfigure;
@@ -19,12 +22,17 @@ public class ProcessMenuBarView extends HBox {
 	private Button btnAutomate;
 	
 	private ProcessMenuBarPresenter presenter;
+	private Translator translator;
 	
 	private Button selectedBtn;
 	
 	private static final String BTN_SELECTED = "selected";
+	private static final int BTN_HEIGHT = 40;
+	private static final int BTN_WIDTH_SMALL = 60;
+	private static final int BTN_WIDTH_LARGE = 120;
 	
 	public ProcessMenuBarView() {
+		translator = Translator.getInstance();
 		buildView();
 	}
 	
@@ -36,14 +44,16 @@ public class ProcessMenuBarView extends HBox {
 		
 		selectedBtn = null;
 		
-		btnAlarms = new Button("Alarms");
+		btnAlarms = new Button(translator.getTranslation("Alarms"));
+		btnAlarms.setPrefSize(BTN_WIDTH_SMALL, BTN_HEIGHT);
 		btnAlarms.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent event) {
 				presenter.showAlarmsView();
 			}
 		});
 		
-		btnAdmin = new Button("Admin");
+		btnAdmin = new Button(translator.getTranslation("Admin"));
+		btnAdmin.setPrefSize(BTN_WIDTH_SMALL, BTN_HEIGHT);
 		btnAdmin.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent event) {
 				presenter.showAdminView();
@@ -53,35 +63,51 @@ public class ProcessMenuBarView extends HBox {
 		hBoxProcessMenuItems = new HBox();
 		hBoxProcessMenuItems.setSpacing(0);
 		
-		btnConfigure = new Button("Configure");
+		btnConfigure = new Button(translator.getTranslation("Configure"));
+		btnConfigure.getStyleClass().add("first");
+		btnConfigure.setPrefSize(BTN_WIDTH_LARGE, BTN_HEIGHT);
 		btnConfigure.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent event) {
 				presenter.showConfigureView();
 			}
 		});
-		btnTeach = new Button("Teach");
+		btnTeach = new Button(translator.getTranslation("Teach"));
+		btnTeach.getStyleClass().add("bar");
+		btnTeach.setPrefSize(BTN_WIDTH_LARGE, BTN_HEIGHT);
 		btnTeach.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent event) {
 				presenter.showTeachView();
 			}
 		});
-		btnAutomate = new Button("Automate");
+		btnAutomate = new Button(translator.getTranslation("Automate"));
+		btnAutomate.setPrefSize(BTN_WIDTH_LARGE, BTN_HEIGHT);
+		btnAutomate.getStyleClass().add("last");
 		btnAutomate.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent event) {
 				presenter.showAutomateView();
 			}
 		});
-		
+				
 		hBoxProcessMenuItems.getChildren().add(btnConfigure);
 		hBoxProcessMenuItems.getChildren().add(btnTeach);
 		hBoxProcessMenuItems.getChildren().add(btnAutomate);
 		
-		getChildren().add(btnAlarms);
-		getChildren().add(hBoxProcessMenuItems);
-		getChildren().add(btnAdmin);
+		buttonBar = new HBox();
 		
-		setHgrow(hBoxProcessMenuItems, Priority.ALWAYS);
+		buttonBar.getChildren().add(btnAlarms);
+		buttonBar.getChildren().add(hBoxProcessMenuItems);
+		buttonBar.getChildren().add(btnAdmin);
+		
+		btnAlarms.setAlignment(Pos.CENTER_LEFT);
+		btnAdmin.setAlignment(Pos.CENTER_RIGHT);
 		hBoxProcessMenuItems.setAlignment(Pos.CENTER);
+		buttonBar.setAlignment(Pos.CENTER);
+		
+		HBox.setHgrow(buttonBar, Priority.ALWAYS);
+		HBox.setHgrow(hBoxProcessMenuItems, Priority.ALWAYS);
+		
+		this.getItems().add(buttonBar);
+		this.setPrefHeight(55);
 	}
 	
 	public void setConfigureActive() {
@@ -110,3 +136,5 @@ public class ProcessMenuBarView extends HBox {
 	}
 	
 }
+
+
---
+++ IRS-CW-Milling/src/eu/robojob/irscw/util/Translator.java
@@ -0,0 +1,37 @@
+package eu.robojob.irscw.util;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Translator {
+	
+	private static Translator instance;
+	private Locale locale;
+	private ResourceBundle messages;
+	
+	private Translator() {
+		setLanguageNL();
+	}
+	
+	public static Translator getInstance() {
+		if (instance==null) {
+			instance = new Translator();
+		}
+		return instance;
+	}
+	
+	public void setLanguageNL() {
+		locale = new Locale("nl");
+		messages = ResourceBundle.getBundle("messages");
+	}
+	
+	public void setLanguageEN() {
+		locale = new Locale("nl");
+		messages = ResourceBundle.getBundle("messages");
+	}
+	
+	public String getTranslation(String key) {
+		return messages.getString(key);
+	}
+	
+}
