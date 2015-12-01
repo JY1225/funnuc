2c8a4bf6c31b721974d21f487fbde5da34427c87
Peter De Roovere <peter.de.roovere@gmail.com>
2015-12-01 16:15:21 +0100

    Changes for dev int v3.00

--- Mill-Assist/src/main/java/eu/robojob/millassist/RoboSoft.java
+++ Mill-Assist/src/main/java/eu/robojob/millassist/RoboSoft.java
@@ -45,6 +45,14 @@ public class RoboSoft extends Application {
 
 	
 	public static void main(final String[] args) {
+	    /*try {
+	        Class<?> macFontFinderClass = Class.forName("com.sun.t2k.MacFontFinder");
+	        Field psNameToPathMap = macFontFinderClass.getDeclaredField("psNameToPathMap");
+	        psNameToPathMap.setAccessible(true);
+	        psNameToPathMap.set(null, new HashMap<String, String>());
+	    } catch (Exception e) {
+	        // ignore
+	    }*/
 		launch(args);
 	}
 	
